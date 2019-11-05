18c790f7f1682420daaab2dca7d60711abc31325
TrihlebDV <trihlebdv@gmail.com>
2019-11-06 02:26:00 +0300

    all the way

---
+++ FR.py
@@ -0,0 +1,10 @@
+import face_recognition
+import cv2
+import numpy as np
+
+class FR(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        pass
